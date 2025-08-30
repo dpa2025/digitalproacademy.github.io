717564b1b721959340e30df2cde4f6fe79c50cc3
dpa2025 <u50896926@gmail.com>
2025-08-31 01:44:03 +0300

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,24 @@
+document.getElementById('contact-form')?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const fd = new FormData(e.target);
+  const name = fd.get('name');
+  const email = fd.get('email');
+  const subject = fd.get('subject');
+  const message = fd.get('message');
+
+  const to = 'hello.digitalproacademy@gmail.com';
+
+  const body = [
+    `Ім'я: ${name}`,
+    `Email: ${email}`,
+    '',
+    `${message}`
+  ].join('%0D%0A');
+
+  const link = `mailto:${to}?subject=${encodeURIComponent(subject)}&body=${body}`;
+
+  const status = document.getElementById('contact-status');
+  status.textContent = 'Відкриваємо ваш поштовий клієнт…';
+  window.location.href = link;
+});
